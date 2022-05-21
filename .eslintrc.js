69f8b92ea38a8524122d6c19e7c7e3455be20f16
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-21 14:05:00 -0500

    3) chore: setting ESLint rules

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true,
+        "jest": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "ignorePatterns": [".eslintrc.js", "*package"],
+    "rules": {
+        indent : ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "multiline-comment-style": ["error", "bare-block"],
+        camelcase: "warn",
+        eqeqeq: ["error", "always"],
+        "func-call-spacing": ["error", "always"],
+        "one-var": ["error", "consecutive"],
+        "no-cond-assign": ["error", "always"],
+        "no-extra-parens": "error",
+        "no-constant-condition": "error",
+        "no-dupe-args": "error"
+    }
+}
