85a6a6b26c841fbae7025fa9414e8269d07b4f3d
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-23 20:55:03 -0500

    9) test: verifying if the file returns an array

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -10,11 +10,17 @@ describe("Test's suite for Reader utility", () => {
         const visualPartnersSourceDataRoute = Reader.readDirectory("src/data");
         expect(visualPartnersSourceDataRoute).not.toBe("ENOENT");
     });
+
+    test("3. Verifying if the file returns an array", () => {
+        const data = Reader.readSourceData("src/data/visualpartners.json");
+        expect(data.constructor === Array).toBeTruthy();
+    });
 });
 
 /*  TODO: Testing if the route file exists
 //  TODO: Testing if the file in the route exists
-//  TODO: Testing if the file returns a not-null array
+//  TODO: Testing if the file returns an array
+//  TODO: Testing if the file returns a not empty array
 //  TODO: Testing if each element of that array is an object
 //  TODO: Testing if every object in the array has all properties.
     TODO: Testing if every value of each object is not undefined
