f656d20151bb0a27dfc78ebb237b7a39e5942b4e
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-25 15:48:13 -0500

    17) feat: Implementing readSourceData(). Unit test included

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     "ignorePatterns": [".eslintrc.js", "*package"],
     "rules": {
-        indent : ["error", 4],
+        indent : ["error", 4, {"ArrayExpression": 1}],
         "linebreak-style": ["error", "unix"],
         quotes: ["error", "double"],
         semi: ["error", "always"],
@@ -19,7 +19,7 @@ module.exports = {
         camelcase: "warn",
         eqeqeq: ["error", "always"],
         "func-call-spacing": ["error", "never"],
-        "one-var": ["error", "consecutive"],
+        //"one-var": ["error", "consecutive"],
         "no-cond-assign": ["error", "always"],
         "no-extra-parens": "error",
         "no-constant-condition": "error",
--- test/services/VisualPartnersService.test.js
+++ test/services/VisualPartnersService.test.js
@@ -1,12 +1,32 @@
 const VisualPartnersService = require("../../lib/services/VisualPartnersService");
 
 describe("Test's suite for VisualPartnerService's class", () => {
-
+    
     test("1. Test if getVisualPartners() returns a list with all data", () => {
-        const data = [{name: "Pepe", id: 2}, {name: "Lola", id: 3}, {name: "Sam", id: 4}], visualPartners = VisualPartnersService.readSourceData(data);
+
+        const data = [
+            {name: "Pepe", id: 2}, 
+            {name: "Lola", id: 3}, 
+            {name: "Sam", id: 4}
+        ];
+        
+        const visualPartners = VisualPartnersService.readSourceData(data);
                 
         expect(visualPartners.length).toBe(3);
         expect(visualPartners).toEqual(data);
+    });
+
+    test("2. Test if the method getVisualPartnersWithCertification() returns a list with explorers certificated", () => {
+
+        const data = [
+            {name: "Pepe", id: 2, haveCertification: true}, 
+            {name: "Lola", id: 3, haveCertification: true}, 
+            {name: "Sam", id: 4, haveCertification: false}
+        ];
+        
+        const visualPartnersWithCertification = VisualPartnersService.filterByCertification(data);
+
+        expect(visualPartnersWithCertification).toStrictEqual([{name: "Pepe", id: 2, haveCertification: true}, {name: "Lola", id: 3, haveCertification: true}]);
     });
 });
 
