271531ae92a7509511a5b2428df75bae820a94c7
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-25 21:31:33 -0500

    21) feat: Implementing filterByCredits() in controller. Unit test included

--- test/controllers/VisualPartnersController.test.js
+++ test/controllers/VisualPartnersController.test.js
@@ -33,9 +33,26 @@ describe("Test's suite for VisualPartnerController", () => {
             ]
         );
     });
+
+    test("3. Checking if filterByCredits() works", () => {
+        const data = [
+            {name: "Pepe Le Puig", id: 2, credits: 450}, 
+            {name: "Lola Bunny", id: 3, credits: 600}, 
+            {name: "Sam Bigotes", id: 4, credits: 500},
+            {name: "Kayley Cuocco", id: 5, credits: 200},
+        ];
+
+        const getVisualPartnersWith500OrMoreCredits = visualPartnersController.filterByCredits(data);
+
+        expect(getVisualPartnersWith500OrMoreCredits).toStrictEqual(
+            [
+                {name: "Lola Bunny", id: 3, credits: 600}, 
+                {name: "Sam Bigotes", id: 4, credits: 500}
+            ]
+        );
+    });
 });
 
 /**
- * TODO: Checking if filterByCertification() works
  * TODO: Checking if filterByCredits() works
  */
