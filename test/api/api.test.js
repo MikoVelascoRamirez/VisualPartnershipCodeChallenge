7cdf3c9f3a9c004f21c28e549e9ab27a246889ac
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-31 13:13:16 -0500

    26) test: testing if status code is 200 of GET /v1/students

---
+++ test/api/api.test.js
@@ -0,0 +1,23 @@
+const app = require("../../lib/utils/api");
+const request = require("supertest");
+
+describe("Suite's tests for visual partners api", () => {
+    test("Test if GET /v1/students returns a 200 status code", async () => {
+        const response = await request(app).get("/v1/students");
+        expect(response.statusCode).toEqual(200);
+    });
+});
+
+/**
+ * TODO: Test if endpoint http://localhost:3000/v1/students returns a 200 status code
+ * TODO: Test if endpoint http://localhost:3000/v1/students responses an object like {response: visualpartners, data: list of visualpartners}
+ * TODO: Test if endpoint http://localhost:3000/v1/students returns a list of all visual partners on data
+ * 
+ * TODO: Test if endpoint http://localhost:3000/v1/emails returns a 200 status code
+ * TODO: Test if endpoint http://localhost:3000/v1/emails responses an object like {response: emailsWithCertification, data: list of certificated}
+ * TODO: Test if endpoint http://localhost:3000/v1/emails returns a list with emails of visual partners with certification on data
+ * 
+ * TODO: Test if endpoint http://localhost:3000/v1/credits returns a 200 status code
+ * TODO: Test if endpoint http://localhost:3000/v1/credits responses an object like {response: credits, data: list of visualpartners with 500 or more credits}
+ * TODO: Test if endpoint http://localhost:3000/v1/credits returns a list of visual partners with 500 or more credits on data
+ */
