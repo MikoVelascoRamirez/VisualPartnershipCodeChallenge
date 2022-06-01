627f47f5f3445b77e6fc1af828bef3f7e87af79e
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-05-31 21:54:07 -0500

    33) test: testing if status code is 200 of GET /v1/credits

--- test/api/api.test.js
+++ test/api/api.test.js
@@ -35,12 +35,15 @@ describe("Suite's tests for visual partners api", () => {
         const data = Reader.readSourceData("test/data/visualPartnersCertificated.json");
         expect(response._body.data).toStrictEqual(data);
     });
+
+    test("Test if GET /v1/credits returns a 200 status code", async () => {
+        const response = await request(app).get("/v1/credits");
+        expect(response.statusCode).toEqual(200);
+    });
 });
 
 
 /**
- * TODO: Test if endpoint http://localhost:3000/v1/emails returns a list with emails of visual partners with certification on data
- * 
  * TODO: Test if endpoint http://localhost:3000/v1/credits returns a 200 status code
  * TODO: Test if endpoint http://localhost:3000/v1/credits responses an object like {response: credits, data: list of visualpartners with 500 or more credits}
  * TODO: Test if endpoint http://localhost:3000/v1/credits returns a list of visual partners with 500 or more credits on data
